c45af950890e2849188dee275d2ce6923df5f721
unknown <gunapriya07>
2024-01-20 17:38:28 +0530

    assignment

---
+++ pronote/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Editor from './component/Editor';
+
+function App() {
+  return (
+    <div className="App">
+      <Editor/>
+
+
+    </div>
+  );
+}
+
+export default App;
